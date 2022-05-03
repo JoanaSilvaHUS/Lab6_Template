474ce62a12154bd1a7fd6c78e2b93ddbbbdd3646
JoanaSilvaHUS <202100190@estudantes.ips.pt>
2022-05-03 14:48:48 -0700

    LAB FEITO

--- main.c
+++ main.c
@@ -11,6 +11,48 @@ int main() {
     }
 
     vector3dPrint(v1, true);
+    free(v1);
+
+    PtVector3d v2 = NULL;
+    vector3dPrint(v1, true);
+    vector3dPrint(v2, true);
+   
+    vector3dDestroy(&v1);
+    vector3dDestroy(&v2); 
     
+    vector3dPrint(v1, true);
+
+    PtVector3d v2 = vector3dCreate(1,0,3);
+    vector3dPrint(v2, true);
+
+    PtVector3d v1a = vector3dMultiplyByScalar(v1, 2.5);
+    vector3dPrint(v1a, true);
+
+    PtVector3d v3 = vector3dAdd(v1, v2);
+    vector3dPrint(v3, true);
+
+    double magnitute;
+    vector3dMagnitude(v1, &magnitute);
+    printf("Magnitude = %.2f \n", magnitute);
+
+
+
     return EXIT_SUCCESS;
+}
+
+void printComponentsOf(PtVector3d vec)
+{
+    double x,y,z;
+
+    if (vector3dX(vec, &x) != VECTOR3D_OK)
+    {
+        printf("Vector NULL. Não é possivel apresentar as componentes.\n");
+    }
+    else
+    {
+        vector3dY(vec, &y);
+        vector3dZ(vec, &z);
+        printf("x = %.2f | y = %.2f | z = %.2f\n", x,y,z);
+    }
+
 }
--- vector3d.c
+++ vector3d.c
@@ -5,33 +5,133 @@
 #include <math.h>
 
 /* TODO: -- define data structure here -- */ 
+    typedef struct vector3d
+    {
+        double x, y, z;
+    }Vector3d;
 
-PtVector3d  vector3dCreate(double x, double y, double z) {
-    return NULL;
+
+    PtVector3d  vector3dCreate(double x, double y, double z) {
+    PtVector3d p = (PtVector3d) malloc(sizeof(Vector3d));
+
+
+    if (p == NULL) return NULL;
+
+    p->x = x;
+    p->y=y;
+    p->z=z;
+
+    return p;
 }
+
 
 void vector3dPrint(PtVector3d vec, bool lineBreakAfter) {
     
+    if(vec == NULL)
+    {
+        printf("NULL");
+    }
+
+    else
+    {
+        printf("<%.21f, %.21f, %.21f>", vec->x, vec->y, vec->z);
+    }
+
+    if(lineBreakAfter)
+    {
+        printf("\n");
+    }
+}
+
+int vector3dDestroy(PtVector3d *pVec)
+{
+    PtVector3d vec = *pVec;
+ 
+    if (vec == NULL) return VECTOR3D_NULL;
+
+    free(vec);
+    *pVec = NULL;
+
+    return VECTOR3D_OK;
+}
+
+PtVector3d vector3dMultiplyByScalar(PtVector3d vec, double scalar)
+{
+    if (vec == NULL) return NULL;
+
+    PtVector3d c = vector3dCreate(vec->x * scalar, vec->y * scalar, vec-> z * scalar);
+
+    return c;
+}
+
+PtVector3d vector3dAss(PtVector3d vec1, PtVector3d vec2)
+{
+    if (vec1 == NULL || vec2 == NULL) return NULL;
+
+    PtVector3d c = vector3dCreate(vec1->x + vec2->x, vec1->y + vec2->y, vec1->y + vec2->z);
+
+    return c;
+}
+
+int vector3dMagnitude(PtVector3d vec, double *magnitude)
+{
+    if (vec == NULL) return VECTOR3D_NULL;
+
+    *magnitude = sqrt(vec->x * vec->x + vec->y * vec->y + vec->z * vec->z);
+
+    return VECTOR3D_OK;
 }
 
 int vector3dX(PtVector3d vec, double *x) {
     
-    return VECTOR3D_OK;
+  if (vec == NULL) return VECTOR3D_NULL;
+
+  *x = vec -> x;
+
+  return VECTOR3D_OK;
+
+
 }
 
 int vector3dY(PtVector3d vec, double *y) {
     
-    return VECTOR3D_OK;
+  if (vec == NULL) return VECTOR3D_NULL;
+
+  *y = vec -> y;
+
+  return VECTOR3D_OK;
 }
 
 int vector3dZ(PtVector3d vec, double *z) {
     
-    return VECTOR3D_OK;
+   if (vec == NULL) return VECTOR3D_NULL;
+
+  *z = vec -> z;
+
+  return VECTOR3D_OK;
 }
 
-int vector3dDestroy(PtVector3d *pVec) {
-    
-    return VECTOR3D_OK;
+PtVector3d vector3dNormalize(PtVector3d vec)
+{
+    if(vec == NULL)
+    {
+        return NULL;
+    }
+    double magnitude = 0;
+    vector3dMagnitude(vec, &magnitude);
+
+    if(magnitude == 0)
+    {
+        return vector3dCreate(0,0,0);
+    }
+    else
+    {
+    PtVector3d c = vector3dCreate(vec->x, vec->y, vec->z);
+    PtVector3d more = vector3dMultiplyByScalar(c, 1/magnitude);
+    }
+
+    free(c);
+    return more;
 }
 
 PtVector3d vector3dMultiplyByScalar(PtVector3d vec, double scalar) {
